a7bdbdf89a4632e9cfcae838dfdf368b36b30420
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2022-10-26 22:32:58 +0200

    Il controllo automatico deve verificare la presenza di un termine del dizionario di riferimento all’interno della password: 1) se il termine del dizionario è presente la password è validata soltanto se nella parte restante della stessa, diversa dal termine incluso nel dizionario, sono rispettati contemporaneamente i seguenti controlli: a) lunghezza pari ad almeno 5 caratteri, posizionati liberamente prima o dopo il termine del dizionario; b) nessun carattere uguale consecutivo; c) almeno 2 caratteri speciali diversi; d) almeno 1 carattere numerico; e) almeno 1 carattere alfabetico con lettera maiuscola; f) almeno 1 carattere alfabetico con lettera minuscola;

--- src/main/java/org/rossonet/keycloak/spi/dictionary/DictionaryCheck.java
+++ src/main/java/org/rossonet/keycloak/spi/dictionary/DictionaryCheck.java
@@ -12,9 +12,14 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+import com.google.common.primitives.Chars;
+
 public class DictionaryCheck {
 
 	public static final String DEFAULT_BLACKLIST_FILE = "parole_italiane.txt";
+
+	public static final char[] SPECIAL_CHARACTERS = new char[] { '\\', '"', '|', '!', '”', '£', '$', '%', '&', '/', '(',
+			')', '=', '?', '’', '^', '[', ']', '*', '@', '#', '°', '_', '-', ':', '.', ';', ',', '<', '>', '€' };
 
 	/*
 	 * Il controllo automatico deve verificare la presenza di un termine del
@@ -29,7 +34,59 @@ public class DictionaryCheck {
 	 *
 	 */
 	public static boolean check(final Set<String> blacklist, final String wordToCheck) {
-		return blacklist.contains(wordToCheck);
+		boolean found = false;
+		for (final String w : blacklist) {
+			if (w.length() < 5) {
+				// non controlla il match
+			} else {
+				if (wordToCheck.toLowerCase().contains(w.toLowerCase())) {
+					if (w.length() + 5 > wordToCheck.length()) {
+						found = true;
+					}
+					for (int i = 0; i < wordToCheck.length() - 1; i++) {
+						if (wordToCheck.charAt(i) == wordToCheck.charAt(i + 1)) {
+							found = true;
+							break;
+						}
+					}
+					int specialCounter = 0;
+					int uppercase = 0;
+					int number = 0;
+					int lowercase = 0;
+					for (int i = 0; i < wordToCheck.length(); i++) {
+						if (Chars.contains(SPECIAL_CHARACTERS, wordToCheck.charAt(i))) {
+							specialCounter++;
+						}
+						if (Character.isUpperCase(wordToCheck.charAt(i))) {
+							uppercase++;
+						}
+						if (Character.isLowerCase(wordToCheck.charAt(i))) {
+							lowercase++;
+						}
+						if (Character.isDigit(wordToCheck.charAt(i))) {
+							number++;
+						}
+					}
+					if (specialCounter < 2) {
+						found = true;
+					}
+					if (uppercase < 1) {
+						found = true;
+					}
+					if (lowercase < 1) {
+						found = true;
+					}
+					if (number < 1) {
+						found = true;
+					}
+				}
+			}
+			if (found) {
+				break;
+			}
+		}
+
+		return found;
 	}
 
 	public static Set<String> loadDictionary(final String dictionaryUrl) throws IOException, MalformedURLException {
--- src/test/java/org/rossonet/keycloak/spi/DictionaryCheckTest.java
+++ src/test/java/org/rossonet/keycloak/spi/DictionaryCheckTest.java
@@ -13,15 +13,35 @@ import org.rossonet.keycloak.spi.dictionary.DictionaryCheck;
 public class DictionaryCheckTest {
 
 	@Test
+	public void checkComplexRules() throws MalformedURLException, IOException {
+		final Set<String> dictionary = DictionaryCheck.loadDictionary(null);
+		System.out.println("found " + dictionary.size() + " words in null");
+		boolean foundInDictionary = DictionaryCheck.check(dictionary, "parola");
+		assertTrue(foundInDictionary);
+		foundInDictionary = DictionaryCheck.check(dictionary, "!Lparola.2A");
+		assertFalse(foundInDictionary);
+		foundInDictionary = DictionaryCheck.check(dictionary, "!lparola.2a");
+		assertTrue(foundInDictionary);
+		foundInDictionary = DictionaryCheck.check(dictionary, "!Lparola.jA");
+		assertTrue(foundInDictionary);
+		foundInDictionary = DictionaryCheck.check(dictionary, "!parola.2A");
+		assertTrue(foundInDictionary);
+		foundInDictionary = DictionaryCheck.check(dictionary, "!LparolaR2A");
+		assertTrue(foundInDictionary);
+		foundInDictionary = DictionaryCheck.check(dictionary, "!LPAROLA.2A");
+		assertTrue(foundInDictionary);
+	}
+
+	@Test
 	public void checkWordInDictionary() throws MalformedURLException, IOException {
 		final Set<String> dictionary = DictionaryCheck.loadDictionary(null);
 		System.out.println("found " + dictionary.size() + " words in null");
-		boolean foundInDictionary = DictionaryCheck.check(dictionary, "sgerro");
+		boolean foundInDictionary = DictionaryCheck.check(dictionary, "sg1erro");
 		assertFalse(foundInDictionary);
 		foundInDictionary = DictionaryCheck.check(dictionary, "cazzata");
 		assertTrue(foundInDictionary);
-		foundInDictionary = DictionaryCheck.check(dictionary, "porcocane");
-		assertFalse(foundInDictionary);
+		foundInDictionary = DictionaryCheck.check(dictionary, "porCocAne");
+		assertTrue(foundInDictionary);
 		foundInDictionary = DictionaryCheck.check(dictionary, "porcocervo");
 		assertTrue(foundInDictionary);
 	}
@@ -32,11 +52,11 @@ public class DictionaryCheckTest {
 				"https://github.com/rossonet/keycloak-password-check-spi/raw/main/src/main/resources/parole_italiane.txt");
 		System.out.println("found " + dictionary.size()
 				+ " words in https://github.com/rossonet/keycloak-password-check-spi/raw/main/src/main/resources/parole_italiane.txt");
-		boolean foundInDictionary = DictionaryCheck.check(dictionary, "sgerro");
+		boolean foundInDictionary = DictionaryCheck.check(dictionary, "sge5rro");
 		assertFalse(foundInDictionary);
 		foundInDictionary = DictionaryCheck.check(dictionary, "cazzata");
 		assertTrue(foundInDictionary);
-		foundInDictionary = DictionaryCheck.check(dictionary, "porcocane");
+		foundInDictionary = DictionaryCheck.check(dictionary, "por.coc.ane");
 		assertFalse(foundInDictionary);
 		foundInDictionary = DictionaryCheck.check(dictionary, "porcocervo");
 		assertTrue(foundInDictionary);
